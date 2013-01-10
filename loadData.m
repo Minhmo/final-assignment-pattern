bb16e4b5a8a37f7d61014e43d9cada83b2eb3ab1
Tamis van der Laan <tamis.vanderlaan@bisc.nl>
2013-01-10 17:35:32 +0100

    dafuq :P

---
+++ loadData.m
@@ -0,0 +1,26 @@
+function [ raw , hog ] = loadData(interval,dim)
+a = prnist([0:9],[1:interval:1000]);
+
+raw = [];
+hog = [];
+
+labels = zeros(1, 250);
+
+period = floor(1000/interval);
+
+for i = 0:9
+    for j = 0:(period-1)
+        index = period * i + j;
+        nist = a(index + 1);
+        im = data2im(nist);
+        imr = imresize(im, dim);
+        raw(index + 1, :) = imr(:);
+        tmp = HOG(imr);
+        hog(index+1,:) = tmp(:);
+        labels(index + 1) = i;
+    end
+end
+raw = dataset(raw, labels');
+hog = dataset(hog, labels');
+end
+
--- main.m
+++ main.m
@@ -9,7 +9,7 @@ if shit_initialized == 0
 end
 
 %array(row, column) so I don' t forget
-
+hogdata = [];
 data = [];
 labels = zeros(1, 250); %zeros(10, 25);
 
@@ -19,14 +19,16 @@ for i = 0:9
         index = 25 * i + j;
         nist = a(index + 1);
         im = data2im(nist);
-        imr = imresize(im, [16, 16]);
+        imr = imresize(im, [32, 32]);
         data(index + 1, :) = imr(:);
+        hog = HOG(imr);
+        hogdata(index+1,:) = hog(:);
         labels(index + 1) = i;
     end
 end
 
-toTrain = dataset(data, labels');
-crossval(toTrain, nmc);
+toTrain = dataset(hogdata, labels');
+crossval(toTrain, knnc([],3));
 
 %nmc(toTrain);
 
